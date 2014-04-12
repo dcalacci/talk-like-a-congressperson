4366d844c66c3d6061d8eff8f43478c65271ea0c
Dan Calacci <dan@dcalacci.net>
2014-04-12 10:46:40 -0400

    removed utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-# utilities for text transformation and stuff like that
-
-
-def get_words_from_file(path):
-    with open(path) as f:
-        data = f.read()
-        words = data.split()
-    return words
