fc38598c90c458926b70d0967c06e0a4ba784a4e
Dan Calacci <dan@dcalacci.net>
2014-04-15 16:32:03 -0400

    Added flask server
     - run 'talk-like-a-congressperson.py' to launch the app
     - takes a bit to start up
     - literally no styling

---
+++ talk-like-a-congressman.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import corpus
+import markov
+from author_mapping import mapping
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html',
+                           congresspeople=mapping.keys())
+
+@app.route('/generate', methods=['GET'])
+def generate():
+    name = request.args.get('congressperson')
+    print "'", name, "'"
+    ngram_length = int(request.args.get('ngram_length'))
+    text_length = int(request.args.get('text_length'))
+    return generate_text(mapping[name], ngram_length, text_length)
+
+@app.route('/congressperson/<int:author_id>')
+def uh_text(author_id):
+    return str(author_id)
+
+
+@app.route('/congressperson/<int:author_id>/generate/<int:ngram>/<int:length>')
+def generate_text(author_id, ngram, length):
+    c = corpus.read_data('corpora/' + str(author_id) + '/corpus.txt',
+                         'vocab.txt')
+    m = markov.Markov(c, ngram)
+    return m.generate(length)
+
+if __name__ == '__main__':
+    app.run(debug=True)
