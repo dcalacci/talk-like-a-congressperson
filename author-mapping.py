b54fc63ffed971d6d8a037b822998e22599c6466
Dan Calacci <dan@dcalacci.net>
2014-04-15 15:12:09 -0400

    removed the main script

---
+++ author-mapping.py
@@ -0,0 +1,15 @@
+#!/usr/bin env python
+# mapping from votesmart IDs to congressperson names
+import yaml
+
+# open the author info from yaml data.
+# this is only data for congresspeople with votesmart IDs.
+stream = open('author-mapping/votesmart.yml', 'rb')
+data = yaml.load(stream)
+
+# create dictionary of Name -> VoteSmart ID
+mapping = [(' '.join([author['name']['first'],
+                      author['name']['last']]),
+            author['id']['votesmart'],) for author in data]
+mapping = dict(mapping)
+
